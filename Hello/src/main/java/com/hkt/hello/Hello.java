c7279cd0e7c35b18cf7440fa189e78f46545f1c3
ngthedung <ngthedung.com@gmail.com>
2015-12-10 16:24:40 +0700

    part 2

--- Hello/src/main/java/com/hkt/hello/Hello.java
+++ Hello/src/main/java/com/hkt/hello/Hello.java
@@ -14,5 +14,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello GitHub");
+        System.out.println("Hello Maven");
     }
 }
